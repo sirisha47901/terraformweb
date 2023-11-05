ec32a6b82b428fb7f18e769353a79031b7caf26d
sirisha47901 <138841067+sirisha47901@users.noreply.github.com>
2023-11-05 19:58:05 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "siri01-rg"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "myappservice-plan"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = "siriapp0201"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  #(Optional)
+  site_config {
+dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+  
+  #(Optional)
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+}
